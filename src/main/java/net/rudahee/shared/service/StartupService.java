10f274a8df4797ee68bd77d873405d6cbf99af95
rudahee <rudahee@gmail.com>
2024-12-30 16:15:35 +0100

    Cors disabled in preprod!

--- src/main/java/net/rudahee/shared/service/StartupService.java
+++ src/main/java/net/rudahee/shared/service/StartupService.java
@@ -25,47 +25,6 @@ public class StartupService implements ApplicationListener<ContextRefreshedEvent
 
     @Override
     public void onApplicationEvent(ContextRefreshedEvent event) {
-        if (shopRepository.count() != 2) {
-            Shop shop = new Shop();
-            shop.setName("Santa Aurelia");
-            shop.setAccumulatedIncome(0D);
-            shop.setAccumulatedExpense(0D);
-            shop.setAccumulatedProfit(0D);
-
-            List<Accounting> accountings1 = new ArrayList<>();
-            Accounting accounting1 = new Accounting();
-            accounting1.setDate(Instant.now());
-            accounting1.setShop(shop);
-            accounting1.setDailyIncome(0D);
-            accounting1.setDailyExpense(0D);
-            accounting1.setDailyProfit(0D);
-            accounting1.setIncomings(null);
-            accounting1.setExpensings(null);
-            accountings1.add(accounting1);
-
-            shop.setAccountings(accountings1);
-            shopRepository.save(shop);
-
-            Shop shop2 = new Shop();
-            shop2.setName("Villegas");
-            shop2.setAccumulatedIncome(0D);
-            shop2.setAccumulatedExpense(0D);
-            shop2.setAccumulatedProfit(0D);
-
-            List<Accounting> accountings2 = new ArrayList<>();
-            Accounting accounting2 = new Accounting();
-            accounting2.setDate(Instant.now());
-            accounting2.setShop(shop);
-            accounting2.setDailyIncome(0D);
-            accounting2.setDailyExpense(0D);
-            accounting2.setDailyProfit(0D);
-            accounting2.setIncomings(null);
-            accounting2.setExpensings(null);
-            accountings2.add(accounting2);
-
-            shop2.setAccountings(accountings2);
-            shopRepository.save(shop2);
-
-        }
+        System.out.println("Initialized!");
     }
 }
